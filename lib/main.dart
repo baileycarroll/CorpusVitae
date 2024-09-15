5b246131cd8b15d0ffada45162e1e9ddba0489ca
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-09-15 11:12:59 -0700

    Initial configuration for shared prefs done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:corpus_vitae/utils/preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,9 @@ import 'screens/goals.dart';
 import 'screens/profile.dart';
 import 'screens/settings.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await sharedPrefs.init();
   runApp(const App());
 }
 
--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -1,1 +1,22 @@
+import 'package:shared_preferences/shared_preferences.dart';
 
+class SharedPrefs {
+  late final SharedPreferences _sharedPrefs;
+
+  static final SharedPrefs _instance = SharedPrefs._internal();
+
+  factory SharedPrefs() => _instance;
+
+  SharedPrefs._internal();
+
+  Future<void> init() async {
+    _sharedPrefs = await SharedPreferences.getInstance();
+  }
+  // Preferences Getters
+  bool get theme => _sharedPrefs.getBool('theme') ?? true;
+  bool get comprehensiveBMI => _sharedPrefs.getBool('comprehensiveBMI') ?? false;
+  // Preferences Setters
+  set theme(bool value) => _sharedPrefs.setBool('theme', value);
+  set comprehensiveBMI(bool value) => _sharedPrefs.setBool('comprehensiveBMI', value);
+}
+final sharedPrefs = SharedPrefs();
