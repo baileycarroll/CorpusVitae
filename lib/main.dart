3eb7843d698405601f1b0b0fd636c7a5f3d7040c
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-09-19 10:10:32 -0700

    Restarting from base

--- lib/main.dart
+++ lib/main.dart
@@ -1,147 +1,125 @@
-import 'package:corpus_vitae/utils/preferences.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'screens/goals.dart';
-import 'screens/home.dart';
-import 'screens/nutrition.dart';
-import 'screens/profile.dart';
-import 'screens/settings.dart';
-import 'screens/workouts.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await sharedPrefs.init();
-  runApp(const App());
+void main() {
+  runApp(const MyApp());
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
-  AppState createState() => AppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
 }
 
-class AppState extends State<App> {
-  @override
-  void initState() {
-    super.initState();
-    sharedPrefs.addListener(_updateTheme);
-  }
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void dispose() {
-    sharedPrefs.removeListener(_updateTheme);
-    super.dispose();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  void _updateTheme() {
-    setState(() {});
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      theme: CupertinoThemeData(
-        brightness: SharedPrefs().theme ? Brightness.dark : Brightness.light,
-        primaryColor: SharedPrefs().theme
-            ? const Color(0xFF22D3EE)
-            : const Color(0xFF6200EE),
-        primaryContrastingColor: SharedPrefs().theme
-            ? const Color(0xFFFFFFFF)
-            : const Color(0xFF000000),
-        textTheme: CupertinoTextThemeData(
-          textStyle: TextStyle(
-            color: SharedPrefs().theme
-                ? const Color(0xFFFFFFFF)
-                : const Color(0xFF000000),
-            fontSize: 16.0,
-          ),
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
-        barBackgroundColor: SharedPrefs().theme
-            ? const Color(0xFF000000)
-            : const Color(0xFFFFFFFF),
-        scaffoldBackgroundColor: SharedPrefs().theme
-            ? const Color(0xFF121212)
-            : const Color(0xFFF5F5F5),
-        applyThemeToAll: true,
       ),
-      home: CupertinoTabScaffold(
-        tabBar: CupertinoTabBar(items: const [
-          BottomNavigationBarItem(
-              icon: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(padding: EdgeInsets.only(top: 1.0)),
-              Icon(CupertinoIcons.home),
-              Text('Home')
-            ],
-          )),
-          BottomNavigationBarItem(
-              icon: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(padding: EdgeInsets.only(top: 1.0)),
-              Icon(Icons.fitness_center),
-              Text('Workouts')
-            ],
-          )),
-          BottomNavigationBarItem(
-              icon: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(padding: EdgeInsets.only(top: 1.0)),
-              Icon(Icons.fastfood),
-              Text('Nutrition')
-            ],
-          )),
-          BottomNavigationBarItem(
-              icon: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(padding: EdgeInsets.only(top: 1.0)),
-              Icon(Icons.flag_circle),
-              Text('Goals')
-            ],
-          )),
-          BottomNavigationBarItem(
-              icon: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(padding: EdgeInsets.only(top: 1.0)),
-              Icon(CupertinoIcons.person),
-              Text('Profile')
-            ],
-          )),
-          BottomNavigationBarItem(
-              icon: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(padding: EdgeInsets.only(top: 1.0)),
-              Icon(CupertinoIcons.settings),
-              Text('Settings')
-            ],
-          ))
-        ]),
-        tabBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return const HomeScreen();
-            case 1:
-              return const WorkoutScreen();
-            case 2:
-              return const NutritionScreen();
-            case 3:
-              return const GoalsScreen();
-            case 4:
-              return const ProfileScreen();
-            case 5:
-              return const SettingsScreen();
-            default:
-              return const HomeScreen();
-          }
-        },
-      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,175 +1,30 @@
-/* Based on the Feature Documentation File in Documentation Folder we want to test for a variety of widgets.
-* 1. Test for the AppBar
-* 2. Test for the Navigation Bar
-* 4. Test for each respective page (Home, Workout Log, Nutrition Log, Goals, Profile, Settings)
-* 5. For the Home Page, test for the following:
-*   a. Test for the Add Workout Button
-*   b. Test for the Add Nutrition Button
-*   c. Test for the Add Goal Button
-*   d. Test for the View Most Recent Workout Button
-*   e. Test for the View Most Recent Meal Button
-* 6. For the Workout Log Page, test for the following:
-*   a. Test for a list of workouts
-*   b. Test for the Add Workout Button
-* 7. For the Nutrition Log Page, test for the following:
-*   a. Test for a list of meals
-*   b. Test for the Add Meal Button
-* 8. For the Goals Page, test for the following:
-*   a. Test for a list of goals
-*   b. Test for the Add Goal Button
-* 9. For the Profile Page, test for the following:
-*   a. Test for the Name
-*   b. Test for the Age
-*   c. Test for the Weight
-*   d. Test for the Height
-*   e. Test for the Sex
-*   f. Test for the Comprehensive BMI Values
-*     i. Test for the BMI Value
-*     ii. Test for the BMR Value
-*     iii. Test for the TDEE Value
-*   g. Test for the Edit Profile Button
-* 10. For the Settings Page, test for the following:
-*   a. Test for the Dark Mode Toggle
-*   b. Test for the Comprehensive BMI Toggle
-*/
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:corpus_vitae/main.dart';
-import 'package:corpus_vitae/screens/profile.dart';
-import 'package:corpus_vitae/screens/settings.dart';
-import 'package:corpus_vitae/utils/preferences.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-Future<void> main() async {
-  // Initialize the database factory
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-  // Shared Preferences
-  SharedPreferences.setMockInitialValues(
-      {'theme': false, 'comprehensiveBMI': false});
-  await sharedPrefs.init();
-  // Test for the App Bar
-  testWidgets('Testing for the App Bar', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.text('Corpus Vitae'), findsOneWidget);
-  });
-  // Test for the Navigation Bar
-  testWidgets('Testing for the Navigation Bar', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.byIcon(CupertinoIcons.home), findsOneWidget);
-    expect(find.byIcon(Icons.fitness_center), findsOneWidget);
-    expect(find.byIcon(Icons.fastfood), findsOneWidget);
-    expect(find.byIcon(Icons.flag_circle), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.person), findsOneWidget);
-    expect(find.byIcon(CupertinoIcons.settings), findsOneWidget);
-  });
-  // Test for the Home Page
-  testWidgets('Testing for the Home Page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    expect(find.byIcon(Icons.add), findsNWidgets(3));
-    expect(find.text('Workout'), findsOneWidget);
-    expect(find.text('Meal'), findsOneWidget);
-    expect(find.text('Goal'), findsOneWidget);
-    expect(find.text('View Most Recent Workout'), findsOneWidget);
-    expect(find.text('View Most Recent Meal'), findsOneWidget);
-    expect(find.byIcon(Icons.remove_red_eye), findsNWidgets(2));
-  });
-  // Test for the Workout Log Page
-  testWidgets('Testing for the Workout Log Page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    await tester.tap(find.byIcon(Icons.fitness_center));
-    await tester.pump();
-    expect(find.byIcon(Icons.add), findsOneWidget);
-    expect(find.byIcon(Icons.remove_red_eye), findsOneWidget);
-    expect(find.text('View All Workouts'), findsOneWidget);
-  });
-  // Test for the Nutrition Log Page
-  testWidgets('Testing for the Nutrition Log Page',
-      (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    await tester.tap(find.byIcon(Icons.fastfood));
-    await tester.pump();
-    expect(find.byIcon(Icons.add), findsOneWidget);
-    expect(find.byIcon(Icons.remove_red_eye), findsOneWidget);
-    expect(find.text('View All Meals'), findsOneWidget);
-  });
-  // Test for the Goals Page
-  testWidgets('Testing for the Goals Page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    await tester.tap(find.byIcon(Icons.flag_circle));
-    await tester.pump();
-    expect(find.byIcon(Icons.add), findsOneWidget);
-    expect(find.byIcon(Icons.remove_red_eye), findsOneWidget);
-    expect(find.text('View All Goals'), findsOneWidget);
-  });
-  // Test for the Profile Page
-  testWidgets('Testing for the Profile Page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    await tester.tap(find.byIcon(CupertinoIcons.person));
-    await tester.pump();
-    expect(find.text('John Doe'), findsOneWidget);
-    expect(find.text('johnd@example.com'), findsOneWidget);
-    expect(find.textContaining('BMI'), findsOneWidget);
-    expect(find.text('Edit Profile'), findsOneWidget);
-  });
-  // Test for the Settings Page
-  testWidgets('Testing for the Settings Page', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
-    await tester.tap(find.byIcon(CupertinoIcons.settings));
-    await tester.pump();
-    expect(find.byType(CupertinoSwitch), findsNWidgets(2));
-    expect(find.text('Dark Mode'), findsOneWidget);
-    expect(find.text('Comprehensive BMI'), findsOneWidget);
-  });
-  // Testing Settings Toggles
-  testWidgets('Testing Dark Mode Toggle', (WidgetTester tester) async {
-    await tester.pumpWidget(const CupertinoApp(home: SettingsScreen()));
+import 'package:corpus_vitae/main.dart';
 
-    // Verify initial state
-    expect(find.byType(CupertinoSwitch), findsNWidgets(2));
-    expect(find.text('Dark Mode'), findsOneWidget);
-    expect(find.text('Comprehensive BMI'), findsOneWidget);
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-    // Toggle Dark Mode
-    await tester.tap(find.byType(CupertinoSwitch).first);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify the change
-    expect(sharedPrefs.theme, true);
-
-    // Verify the theme change
-    await tester.pumpWidget(CupertinoApp(
-      theme: CupertinoThemeData(
-        brightness: sharedPrefs.theme ? Brightness.dark : Brightness.light,
-      ),
-      home: const ProfileScreen(),
-    ));
-    final BuildContext context = tester.element(find.byType(ProfileScreen));
-    expect(CupertinoTheme.of(context).brightness, Brightness.dark);
-  });
-
-  // Test for the Comprehensive BMI Toggle
-  testWidgets('Testing Comprehensive BMI Toggle', (WidgetTester tester) async {
-    await tester.pumpWidget(const CupertinoApp(home: SettingsScreen()));
-
-    // Verify initial state
-    expect(find.byType(CupertinoSwitch), findsNWidgets(2));
-    expect(find.text('Dark Mode'), findsOneWidget);
-    expect(find.text('Comprehensive BMI'), findsOneWidget);
-
-    // Toggle Comprehensive BMI
-    await tester.tap(find.byType(CupertinoSwitch).last);
-    await tester.pump();
-
-    // Verify the change
-    expect(sharedPrefs.comprehensiveBMI, true);
-
-    // Verify the BMI values change
-    await tester.pumpWidget(const CupertinoApp(home: ProfileScreen()));
-    expect(find.textContaining('BMR'), findsOneWidget);
-    expect(find.textContaining('TDEE'), findsOneWidget);
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
